2bd6f341e8b5902e3aee4dcf78b1a521a0aecb7a
operats <mingwah.tham@gmail.com>
2024-07-15 16:19:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -79,9 +79,10 @@ if st.button("Predict"):
     # Process trans_date_trans_time
     input_df['date'] = pd.to_datetime(input_df['trans_date'])
     input_df['days'] = input_df['date'].dt.day_name()
+    input_df['hour'] = input_df['trans_time'].hour
     #input_df['time'] = pd.to_datetime(input_df['trans_time'])
     #input_df['hour'] = input_df['time'].dt.hour
-    input_df['hour'] = input_df['trans_time'].hour
+
     # Delete the original category columns
     input_df.drop(columns=['trans_date','trans_time','date'], inplace=True)
     #input_df.drop(columns=['trans_date','trans_time','date','time'], inplace=True)
