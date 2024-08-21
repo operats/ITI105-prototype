61ec07b8296fa91f2ac95d724082137f795950c8
operats <mingwah.tham@gmail.com>
2024-08-21 15:33:23 +0800

    Update run.py

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ output = "model.pkl"
 gdown.download(url, output)
 
 # Load the RF model
-model_rf = joblib.load("model.pkl")
+model_rf = joblib.load("/mount/src/iti105-prototype/model.pkl")
 
 # Load the model and encoders
 model = joblib.load("LogisticRegression_small.joblib")
